70d88ee6fb15cba23523e2f1ac0059309240db42
Riley <rwaugh52@gmail.com>
2022-04-08 18:43:39 -0700

    Adding popoutWindow

--- youtube-popout-demo/src/app/app.module.ts
+++ youtube-popout-demo/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PopoutWindowModule } from 'angular-opinionated-popout-window';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +25,8 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    PopoutWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
