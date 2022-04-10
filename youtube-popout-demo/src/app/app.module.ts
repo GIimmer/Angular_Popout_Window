42c8aff9bd8eba6cdd8e12b9ae604628132c84af
Riley <rwaugh52@gmail.com>
2022-04-10 01:40:58 -0700

    Final preparations for demo

--- youtube-popout-demo/src/app/app.module.ts
+++ youtube-popout-demo/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { PopoutWindowModule } from 'angular-opinionated-popout-window';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list'; 
+import { MatButtonModule } from '@angular/material/button'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { ClockComponent } from './clock/clock.component';
 import { TextDemoComponent } from './text-demo/text-demo.component';
 import { RandomColorGeneratorComponent } from './random-color-generator/random-color-generator.component';
-import { MatListModule } from '@angular/material/list'; 
-import { MatButtonModule } from '@angular/material/button'; 
 
 @NgModule({
   declarations: [
